76262faab71dd07e90648f85666b541c0591d338
V-Carvalho <vinicius.a.carvalho@outlook.com>
2023-02-10 17:05:04 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
+const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 const router = require("./router/animals.router");
 app.use("/", router);
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Servidor iniciado na porta 3000");
 });
